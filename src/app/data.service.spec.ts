c70d8a723040baa2d9daf56ddf10199790b67887
Edward <e.kirakosyan@andersenlab.com>
2024-02-11 14:43:31 +0400

    c

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,16 +1,36 @@
 import { TestBed } from '@angular/core/testing';
+import { USERS } from './mock-data/users';
 
 import { DataService } from './data.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('DataService', () => {
   let service: DataService;
+  let testingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(DataService);
+    testingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all users function', () => {
+    service.getAllUsers().subscribe((users: any) => {
+      expect(users).toBeTruthy();
+      expect(users.length).toBe(4);
+      const secUser = users.find((user: any) => user.id === 2); // Corrected usage of find method
+      expect(secUser.name).toBe('dsacsd');
+    });
+    const mockReq = testingController.expectOne('api/users'); // Corrected mock request URL
+    mockReq.flush(Object.values(USERS));
   });
 });
---
+++ src/app/mock-data/users.ts
@@ -0,0 +1,26 @@
+export const USERS = {
+  1: {
+    id: 1,
+    name: 'yyyyy',
+    age: 22,
+    gender: 'male',
+  },
+  2: {
+    id: 2,
+    name: 'dsacsd',
+    age: 55,
+    gender: 'female',
+  },
+  3: {
+    id: 3,
+    name: 'gnbgn',
+    age: 7,
+    gender: 'male',
+  },
+  4: {
+    id: 4,
+    name: 'kkk',
+    age: 99,
+    gender: 'female',
+  },
+};
